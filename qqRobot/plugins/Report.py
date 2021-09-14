ec0e24ee37f0e7d14678312cc19e04a47e1abae9
戴林峰 <dailinfeng66@163.com>
2021-09-14 11:24:59 +0800

    fix

--- qqRobot/plugins/Report.py
+++ qqRobot/plugins/Report.py
@@ -12,18 +12,17 @@ frend_status = on_command("研友统计", priority=5)
 async def frend_status_(bot: Bot, event: Event, state: T_State):
     res = requests.get("http://10.23.71.70:5700/get_group_member_list?group_id=343659014").text
     res_j = json.loads(res)
-    # print(res_j)
-    count = 0
     res_data = {
         "计算机": 0,
+        "人工智能": 0,
         "网安": 0,
         "软工": 0,
+        "区块链": 0,
         "电子信息": 0,
         "22级群友总共": 0
     }
     for member in res_j['data']:
         if "22" in member['card']:
-            count += 1
             if "计算机" in member['card'] or "计科" in member['card'] or "计专" in member['card']:
                 res_data['计算机'] += 1
             if "软工" in member['card'] or "软件工程" in member['card'] or "软" in member['card']:
@@ -32,8 +31,10 @@ async def frend_status_(bot: Bot, event: Event, state: T_State):
                 res_data['网安'] += 1
             if "电子信息" in member['card']:
                 res_data['电子信息'] += 1
-            if "22" in member['card']:
-                res_data['22级群友总共'] += 1
-            # print(member['card'])
+            if "人工智能" in member['card'] or "人智" in member['card']:
+                res_data['人工智能'] += 1
+            if "区块链" in member['card']:
+                res_data['区块链'] += 1
+            res_data['22级群友总共'] += 1
 
     await frend_status.finish(str(res_data))
